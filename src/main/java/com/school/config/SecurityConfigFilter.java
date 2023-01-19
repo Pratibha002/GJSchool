ea477118c5650fa32b0945eb52f1ed9c694e366c
Aman Singh Tomar <amansinghtomar2209@gmail.com>
2023-01-20 03:26:08 +0530

    Spring security customAuthorization Flow removed // Now FormBased Authentication Enabled

--- src/main/java/com/school/config/SecurityConfigFilter.java
+++ src/main/java/com/school/config/SecurityConfigFilter.java
@@ -29,30 +29,5 @@ public class SecurityConfigFilter extends WebSecurityConfigurerAdapter {
 	//	auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(NoOpPasswordEncoder.getInstance());
 	}
 
-	// ByDefault formLogin and Basic http login are activated
-	// Here we are making custom authorization flow
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
 	
-		http.authorizeRequests().antMatchers(HttpMethod.POST,"/processPhoto","/saveAdmissison").permitAll().and().csrf().disable();
-		
-//	http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); //Basic Http authorization
-//	http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/customLogin").and().csrf().disable(); //FormLogin authorization
-//	http.authorizeRequests().anyRequest().denyAll(); //will deny all end points
-
-//	http.authorizeRequests().anyRequest().permitAll(); //will deny all end points
-/*
-	 Securing one or multiple endpoints
-		 authenticate() + formLogin() / httpBasic() -> Combination
-*/
-//		http.authorizeRequests().antMatchers("/stulist").authenticated().and().formLogin().loginPage("/customlogin").and().logout().and().exceptionHandling().accessDeniedPage("/accessdenied");
- 
-//		http.authorizeRequests().antMatchers("/stulist").hasAnyAuthority("user").and().formLogin().loginPage("/customlogin").and().logout();
-//		http.authorizeRequests().antMatchers("/studentsList").authenticated().and().formLogin(); //authorize only /studentsList endpoint
-//		http.authorizeRequests().antMatchers("/stulist").authenticated().antMatchers("/contact").permitAll().and().formLogin();
-		
-	//	http.authorizeRequests().anyRequest().authenticated().and().formLogin(); // Authorize all request
-	//	http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/customlogin"); // Authorize all request
-
-	}
 }
